fa58c072eca7c32c40247b5aecb503eb7dea8c8c
AshwinWagh3 <97375569+AshwinWagh3@users.noreply.github.com>
2022-12-12 17:31:24 +0530

    Add files via upload

---
+++ Commonthings.java
@@ -0,0 +1,12 @@
+package com.Commonthings;
+
+public class Commonthings {
+   
+	
+	public static String client_id="77bd7hgzq01lvb";
+	public static String redirect_url="http://localhost:8080/success";
+	public static String client_secret="Q6kNsIMG5lpg0s72";
+
+
+	
+}
